f1e334e75d9a52920480c15065c6c999a20a359a
Jevgenijs <jeff@loriena.lv>
2022-04-26 01:16:43 +0300

    new

---
+++ Lesson3/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Lesson3
+{
+	public class Person
+	{
+
+		public Person(string name)
+        {
+
+			Name = name;
+
+        }
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		public bool IsMale { get; set; }
+
+
+		public void Greeting()
+        {
+
+			Console.WriteLine($"Hello, My name is: {Name} and I am {Age} old");
+
+        }
+
+
+
+	}
+}
+
