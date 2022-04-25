49b89cb0ab1c7042a92de62054a9f0da59dc5059
Jevgenijs <jeff@loriena.lv>
2022-04-25 23:55:17 +0300

    updated

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("THIS IS NEW TEST");
+
+string newVariable;
+newVariable = "eradfdafad";
+
+Console.WriteLine("Let's try something new: " + newVariable);
+
+double newNumber = 12.4554534534;
+Console.WriteLine(newNumber);
+
+
